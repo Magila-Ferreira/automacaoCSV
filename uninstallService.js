1085033e4dc256da726671129cbb0005854c0194
Mágila Morgana da Silva Ferreira <magilamorganasf@hotmail.com>
2025-03-20 17:34:52 -0300

    Script de desinstalação do serviço

--- uninstallService.js
+++ uninstallService.js
@@ -1,18 +1,11 @@
 import { Service } from 'node-windows';
 
-// Criar o serviço com o mesmo nome usado na instalação
 const svc = new Service({
 	name: "AutomacaoNodeJS"
 });
 
-// Verificar se o serviço está instalado antes de remover
 svc.on("uninstall", () => {
 	console.log("Serviço removido com sucesso!");
 });
 
-svc.on("alreadyuninstalled", () => {
-	console.log("O serviço já estava desinstalado.");
-});
-
-// Executa a desinstalação
 svc.uninstall();
