f1b64a62d22b8b12a304c384416daebc3ab9caae
Mágila Morgana da Silva Ferreira <magilamorganasf@hotmail.com>
2026-05-24 20:17:22 -0300

    Caminho da pasta de destino <pdf>

--- alerta/mostrar_alerta.bat
+++ alerta/mostrar_alerta.bat
@@ -4,4 +4,4 @@ echo "Arquivos PDF salvos com sucesso!"
 echo "Abrindo a pasta de destino... em 3 segundos."
 
 timeout /t 3 >nul
-explorer "C:\amb_sw\arquivosPgr\pdf"
+explorer "E:\DEV_2025\arquivosPgr\pdf"
